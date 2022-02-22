dfe5fabcd1abebc37a5e584cd8109769119130dd
sedian <diestrasergio@gmail.com>
2022-02-22 14:48:52 -0500

    task route added

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,6 +3,10 @@ import controller from "../controllers/taskController.js";
 
 const router = express.Router();
 
-
+router.post("/registerTask", controller.registerTask);
+router.get("/listTaskUser/:_id", controller.listTask);
+router.get("/listTask", controller.listTaskAdmin);
+router.put("/updateTask", controller.updateTask);
+router.delete("/deleteTask", controller.deleteTask);
 
 export default router;
